8aa0f28a03c02bf8cdeac83ce5562032a8040175
syed wakil <syedawakil@gmail.com>
2020-09-06 15:35:16 +1000

    added the calculator rounded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { v4 as uuid } from 'uuid';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { v4 as uuid } from "uuid";
 
-
-import { AppState } from './store/models/app-state.model';
-import { TimesheetItem } from './store/models/timesheet-item.model';
-import { AddItemAction, DeleteItemAction, LoadTimesheetAction, UpdateItemAction } from './store/actions/timesheet.actions';
+import { AppState } from "./store/models/app-state.model";
+import { TimesheetItem } from "./store/models/timesheet-item.model";
+import {
+  AddItemAction,
+  DeleteItemAction,
+  LoadTimesheetAction,
+  UpdateItemAction,
+} from "./store/actions/timesheet.actions";
 
 import {
   FormBuilder,
@@ -17,37 +21,48 @@ import {
   Validators,
 } from "@angular/forms";
 
-import {getTimesheetEntryForm} from './forms/timesheet-entry.form'
+import { getTimesheetEntryForm } from "./forms/timesheet-entry.form";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
   timesheetEntryForm: FormGroup;
-  
+
   timesheetItems: Observable<Array<TimesheetItem>>;
   loading$: Observable<Boolean>;
-  error$: Observable<Error>
-  newTimesheetItem: TimesheetItem = { id: '', name: '', timesheetDate: '', timesheetTitle: '', timesheetType: '', timesheetDuration: '', timesheetRate: '250.50', timesheetDayTotal: '', isSubmit: false, isEdit: false, state: 'Active'  }
+  error$: Observable<Error>;
+  newTimesheetItem: TimesheetItem = {
+    id: "",
+    name: "",
+    timesheetDate: "",
+    timesheetTitle: "",
+    timesheetType: "",
+    timesheetDuration: "",
+    timesheetRate: "250.50",
+    timesheetDayTotal: "",
+    isSubmit: false,
+    isEdit: false,
+    state: "Active",
+  };
 
   isNew = false;
-  txtUpdate = 'Edit';
-  selectedItem = '';
+  txtUpdate = "Edit";
+  selectedItem = "";
   itemforSubmit = {};
-  taskTypes = ['Telephone Call', 'Drafting document', 'Research'];
-  defaultType = 'Research';
+  taskTypes = ["Telephone Call", "Drafting document", "Research"];
+  defaultType = "Research";
 
-
-  constructor(private store: Store<AppState>, private fb: FormBuilder,) { }
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.timesheetEntryForm = getTimesheetEntryForm(this.fb);
     this.timesheetEntryForm.patchValue({ timesheetHourlyRate: "250.50" });
-    this.timesheetItems = this.store.select(store => store.timesheet.list);
-    this.loading$ = this.store.select(store => store.timesheet.loading);
-    this.error$ = this.store.select(store => store.timesheet.error);
+    this.timesheetItems = this.store.select((store) => store.timesheet.list);
+    this.loading$ = this.store.select((store) => store.timesheet.loading);
+    this.error$ = this.store.select((store) => store.timesheet.error);
 
     this.store.dispatch(new LoadTimesheetAction());
   }
@@ -70,100 +85,171 @@ export class AppComponent implements OnInit {
   get timesheetDuration() {
     return this.timesheetEntryForm.get("timesheetDuration");
   }
-  
-  
-  
 
   onNewClick(event) {
-    this.isNew= true;
-
+    this.isNew = true;
   }
-  editTimesheetItem(event, timesheet){
+  editTimesheetItem(event, timesheet) {
     timesheet.isEdit = true;
     // this.defaultType = timesheet.timesheetType;
-
-
-
   }
-  onOptionsSelected(val, timesheet){
+  onOptionsSelected(val, timesheet) {
     console.log(val);
     timesheet.timesheetType = val;
-
-
   }
-  updateTimesheetItem(event, timesheet){
-    
-      const updatedTimesheetItem = { id: timesheet.id, name: name, timesheetDate: timesheet.timesheetDate, timesheetTitle: timesheet.timesheetTitle, timesheetType: timesheet.timesheetType, timesheetDuration: timesheet.timesheetDuration, timesheetRate: timesheet.timesheetRate, timesheetDayTotal: timesheet.timesheetDayTotal, isSubmit: false, isEdit: false, state: 'Active'   };
-    
-      this.store.dispatch(new UpdateItemAction(updatedTimesheetItem));
-      timesheet.isEdit = false;
-    
+  updateTimesheetItem(event, timesheet) {
+    const updatedTimesheetItem = {
+      id: timesheet.id,
+      name: name,
+      timesheetDate: timesheet.timesheetDate,
+      timesheetTitle: timesheet.timesheetTitle,
+      timesheetType: timesheet.timesheetType,
+      timesheetDuration: timesheet.timesheetDuration,
+      timesheetRate: timesheet.timesheetRate,
+      timesheetDayTotal: timesheet.timesheetDayTotal,
+      isSubmit: false,
+      isEdit: false,
+      state: "Active",
+    };
 
+    this.store.dispatch(new UpdateItemAction(updatedTimesheetItem));
+    timesheet.isEdit = false;
   }
 
-  onCancel(event){
-    this.timesheetEntryForm.patchValue({ timesheetTitle: "", timesheetType: "" });
+  onCancel(event) {
+    this.timesheetEntryForm.patchValue({
+      timesheetTitle: "",
+      timesheetType: "",
+    });
     this.isNew = false;
-
-
   }
 
   addItem() {
-  this.newTimesheetItem.id = uuid();
-  this.newTimesheetItem.timesheetTitle = this.timesheetEntryForm.value.timesheetTitle;
-  this.newTimesheetItem.timesheetType = this.timesheetEntryForm.value.timesheetType;
-  this.newTimesheetItem.timesheetRate = this.timesheetEntryForm.value.timesheetHourlyRate;
-  this.newTimesheetItem.timesheetDayTotal = this.timesheetEntryForm.value.timesheetDayTotal;
-  this.newTimesheetItem.timesheetDate = this.timesheetEntryForm.value.timesheetDate;
-  this.newTimesheetItem.timesheetDuration = this.timesheetEntryForm.value.timesheetDuration;
-  
-  console.log('timesheetitem', this.newTimesheetItem);
+    this.newTimesheetItem.id = uuid();
+    this.newTimesheetItem.timesheetTitle = this.timesheetEntryForm.value.timesheetTitle;
+    this.newTimesheetItem.timesheetType = this.timesheetEntryForm.value.timesheetType;
+    this.newTimesheetItem.timesheetRate = this.timesheetEntryForm.value.timesheetHourlyRate;
+    this.newTimesheetItem.timesheetDayTotal = this.timesheetEntryForm.value.timesheetDayTotal;
+    this.newTimesheetItem.timesheetDate = this.timesheetEntryForm.value.timesheetDate;
+    this.newTimesheetItem.timesheetDuration = this.timesheetEntryForm.value.timesheetDuration;
+
+    console.log("timesheetitem", this.newTimesheetItem);
 
     this.store.dispatch(new AddItemAction(this.newTimesheetItem));
 
-    this.newTimesheetItem = { id: '', name: '', timesheetDate: '', timesheetTitle: '', timesheetType: '', timesheetDuration: '', timesheetRate: '', timesheetDayTotal: '', isSubmit: false, isEdit: false, state: 'Active'  };
+    this.newTimesheetItem = {
+      id: "",
+      name: "",
+      timesheetDate: "",
+      timesheetTitle: "",
+      timesheetType: "",
+      timesheetDuration: "",
+      timesheetRate: "",
+      timesheetDayTotal: "",
+      isSubmit: false,
+      isEdit: false,
+      state: "Active",
+    };
     this.timesheetEntryForm.patchValue({ timesheetTitle: "" });
     this.timesheetEntryForm.patchValue({ timesheetType: "" });
-    this.isNew=false;
-
+    this.isNew = false;
   }
-  // updateItem(id: string, name: string) {
-  //   const updatedTimesheetItem = { id: id, name: name, timesheetTitle: '', timesheetType: '' , isSubmit: false, isEdit: false, state: 'Active'   };
-    
-  //     this.store.dispatch(new UpdateItemAction(updatedTimesheetItem));
   
-  //     //this.newTimesheetItem = { id: '', name: '' };
-  //   }
 
   deleteItem(id: string) {
     this.store.dispatch(new DeleteItemAction(id));
   }
 
   editItem(id: string, name: string) {
-    this.newTimesheetItem = { id: id , name: name, timesheetDate: '',  timesheetTitle: '', timesheetType: '', timesheetDuration: '', timesheetRate: '', timesheetDayTotal: '',  isSubmit: false, isEdit: false , state: 'Active'   };
+    this.newTimesheetItem = {
+      id: id,
+      name: name,
+      timesheetDate: "",
+      timesheetTitle: "",
+      timesheetType: "",
+      timesheetDuration: "",
+      timesheetRate: "",
+      timesheetDayTotal: "",
+      isSubmit: false,
+      isEdit: false,
+      state: "Active",
+    };
   }
 
   onSelectedRow(timesheetItem) {
-    if(this.selectedItem === timesheetItem.id) {
-      this.selectedItem ='';
-
+    if (this.selectedItem === timesheetItem.id) {
+      this.selectedItem = "";
     } else {
       this.selectedItem = timesheetItem.id;
       this.itemforSubmit = timesheetItem;
     }
-    
-
   }
 
-  onItemSubmit(timesheet){
-    if(this.selectedItem !=='') {
-      console.log('ready for submit', timesheet);
-      const updatedTimesheetItemforSubmit = { id: timesheet.id, name: name, timesheetDate: timesheet.timesheetDate,  timesheetTitle: timesheet.timesheetTitle, timesheetType: timesheet.timesheetType, timesheetDuration: timesheet.timesheetDuration, timesheetRate: timesheet.timesheetRate, timesheetDayTotal: timesheet.timesheetDayTotal, isSubmit: false, isEdit: false, state: 'Submitted'   };
+  onItemSubmit(timesheet) {
+    if (this.selectedItem !== "") {
+      console.log("ready for submit", timesheet);
+      const updatedTimesheetItemforSubmit = {
+        id: timesheet.id,
+        name: name,
+        timesheetDate: timesheet.timesheetDate,
+        timesheetTitle: timesheet.timesheetTitle,
+        timesheetType: timesheet.timesheetType,
+        timesheetDuration: timesheet.timesheetDuration,
+        timesheetRate: timesheet.timesheetRate,
+        timesheetDayTotal: timesheet.timesheetDayTotal,
+        isSubmit: false,
+        isEdit: false,
+        state: "Submitted",
+      };
       this.store.dispatch(new UpdateItemAction(updatedTimesheetItemforSubmit));
-      timesheet.state = 'Submitted';
-      this.selectedItem = '';
-
-
+      timesheet.state = "Submitted";
+      this.selectedItem = "";
     }
+  }
+
+  calculateRate(event) {
+    console.log("cal", event.target);
+    if (this.timesheetEntryForm.valid) {
+      let tot: number;
+      tot = this.calculateTotal(
+        this.timesheetEntryForm.value.timesheetDuration,
+        this.timesheetEntryForm.value.timesheetHourlyRate
+      );
+      this.timesheetEntryForm.patchValue({ timesheetDayTotal: tot.toFixed(2) });
+    }
+  }
+
+  calculateUpdatedRate(event, timesheet) {
+    timesheet.timesheetDayTotal = this.calculateTotal(
+      timesheet.timesheetDuration,
+      timesheet.timesheetRate
+    ).toFixed(2);
+  }
+
+  calculateTotal(duration, rate) {
+    let numarray = duration.split(":");
+    let a = new Array();
+    a = numarray;
+    let hours: number = parseFloat(a[0]);
+    let minutes: number = parseFloat(a[1]);
+    let fract: number = 0.0;
+    if (minutes > 0 && minutes <= 15) {
+      fract = 0.25;
+    } else if (minutes > 15 && minutes <= 30) {
+      fract = 0.5;
+    } else if (minutes > 30 && minutes <= 45) {
+      fract = 0.75;
+    } else {
+      fract = 1.0;
+    }
+
+    let roundedHours: number = fract + hours;
+    let total: number = 0.0;
+    total = roundedHours * parseFloat(rate);
+    console.log("total", total);
+
+    console.log(a[0]);
+    console.log(a[1]);
+    return total;
   }
 }
